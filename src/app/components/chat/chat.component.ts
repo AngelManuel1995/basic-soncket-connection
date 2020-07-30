d77512be3d6218ab63f92d5aab5ffa5b053825d9
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2020-07-29 23:11:53 -0500

    Chat online completed

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -8,18 +8,24 @@ import { ChatService } from 'src/app/services/chat.service';
 })
 export class ChatComponent implements OnInit {
   public text: ''
-  public messages: Array<string> = []
+  public messages: Array<any> = []
+  public messagesContainerElement: HTMLElement
   constructor( public chatService:ChatService ) { }
 
   ngOnInit(): void {
+    this.messagesContainerElement = document.getElementById('chat-messages-container')
     this.chatService.getMessage().subscribe((message:any) => {
-      console.log(message)
       this.messages.push(message)
+      setTimeout(() => {
+        this.messagesContainerElement.scrollTop = this.messagesContainerElement.scrollHeight
+      }, 50);
     })
   }
 
   sendMessage(){
-    console.log(this.text)
+    if(this.text.trim().length === 0){
+      return
+    }
     this.chatService.sendMessage(this.text)
     this.text = ''
   }
